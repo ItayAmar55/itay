66ac5e591b62eab7b192645577283aaa734667a1
itay <itayamar206@gmail.com>
2023-08-16 14:22:55 +0300

    1.1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello change")
+#1
+print(" Hello Enter your  name: itay ")
+
+#2
+print(" Hello Enter your adress: Ashkelon ")
+
+
+
+"""
+name = "Itay" 
+print(" Hello " , name ," How are you: " )
+"""
